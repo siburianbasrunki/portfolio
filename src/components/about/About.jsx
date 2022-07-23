851a25bcb21df826e4ce5fb0eb5d867e72859ecb
siburianbasrunki <basrunkisiburian@gmail.com>
2022-07-23 13:56:54 +0700

    oke

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -22,7 +22,7 @@ const About = () => {
             <article className='about__card'>
               <FaAward className='about__icon'/>
               <h5>Pengalaman</h5>
-              <small>+- 1 Tahun</small>
+              <small>1 Tahun</small>
             </article>
             <article className='about__card'>
               <TbFileCertificate className='about__icon'/>
